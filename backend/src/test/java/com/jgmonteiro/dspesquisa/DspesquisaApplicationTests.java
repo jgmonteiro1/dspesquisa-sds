0a11f7e6ac05ccb28e2cf7131b902086e2fcbe85
João Gabriel <jgmoreira15@gmail.com>
2020-09-15 21:45:33 -0300

    Projeto criado

---
+++ backend/src/test/java/com/jgmonteiro/dspesquisa/DspesquisaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jgmonteiro.dspesquisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DspesquisaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
