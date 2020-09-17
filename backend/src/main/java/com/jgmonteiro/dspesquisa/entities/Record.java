fa7f6b9a0ab7e7f8dada502efebb6ec3ccea5a13
João Gabriel <jgmoreira15@gmail.com>
2020-09-17 00:29:35 -0300

    Mapeamento objeto relacional utilizando JPA

--- backend/src/main/java/com/jgmonteiro/dspesquisa/entities/Record.java
+++ backend/src/main/java/com/jgmonteiro/dspesquisa/entities/Record.java
@@ -2,16 +2,30 @@ package com.jgmonteiro.dspesquisa.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
-//Classe responsável por salvar o registro da pesquisa
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_record")
 public class Record implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue( strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private Integer age;
 	private Instant moment;
-	//Associacao com game
+	
+	@ManyToOne
+	@JoinColumn(name = "game_id")
 	private Game game;
 	
 	public Record() {
