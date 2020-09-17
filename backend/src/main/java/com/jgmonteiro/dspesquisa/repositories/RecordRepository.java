a8dff7bbf87c871d5afcbc27ad11dce4c85e203f
João Gabriel <jgmoreira15@gmail.com>
2020-09-17 01:27:32 -0300

    Repositories adicionados

---
+++ backend/src/main/java/com/jgmonteiro/dspesquisa/repositories/RecordRepository.java
@@ -0,0 +1,11 @@
+package com.jgmonteiro.dspesquisa.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.jgmonteiro.dspesquisa.entities.Record;
+
+@Repository
+public interface RecordRepository extends JpaRepository<Record, Long>{
+
+}
